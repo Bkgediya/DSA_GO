e114c230dae5b17019a25574bc47a181ef37ed84
Bhavdip Gediya <bhavdipgediya099@gmail.com>
2025-09-23 21:05:02 +0530

    merging communities concept

---
+++ graphs/merging_communities.go
@@ -0,0 +1,9 @@
+package graph
+
+// pattern: disjoint set
+
+// when to use 
+// asked to merge groups dynamically.
+// if two nodes belong to the same group.
+// need properties of connected components (size, leader, etc.).
+// Problems like: Friend circles, merging communities, accounts merge, network connectivity.
