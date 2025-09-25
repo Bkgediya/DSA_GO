64492bfaaef02eb53c9b04ad6d955ca548930b3a
Bhavdip Gediya <bhavdipgediya099@gmail.com>
2025-09-25 22:03:08 +0530

    disjoint set union function

--- graphs/merging_communities.go
+++ graphs/merging_communities.go
@@ -2,8 +2,15 @@ package graph
 
 // pattern: disjoint set
 
-// when to use 
+// when to use
 // asked to merge groups dynamically.
 // if two nodes belong to the same group.
 // need properties of connected components (size, leader, etc.).
 // Problems like: Friend circles, merging communities, accounts merge, network connectivity.
+
+// Union-Find
+// Union: merge two groups.
+
+func UnionFind() {
+
+}
